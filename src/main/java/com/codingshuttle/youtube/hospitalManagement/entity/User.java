29d0d0180a9ad3f2db81d6fd81cc1418eae88b0b
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2026-02-27 09:03:33 +0530

    Getting user details from database

---
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/entity/User.java
@@ -0,0 +1,34 @@
+package com.codingshuttle.youtube.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(unique = true)
+    private String username;
+
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}
