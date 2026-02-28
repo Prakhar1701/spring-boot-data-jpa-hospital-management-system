c0e28eda678ae6b0aa91d2d51061eaddda9f95a1
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2026-02-28 13:03:36 +0530

    Getting response from authenticated api using JWT token - add JwtAuthFilter

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
@@ -9,12 +9,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -26,9 +29,10 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/public/**", "/auth/**").permitAll()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/doctors/**").hasAnyRole("DOCTORS", "ADMIN")
-                );
+//                                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                                .requestMatchers("/doctors/**").hasAnyRole("DOCTORS", "ADMIN")
+                                .anyRequest().authenticated()
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 //                .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
